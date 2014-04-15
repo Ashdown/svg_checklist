7b8a14609101103849bf4b36176d07fa72fe5c3e
Rory Devane <rory.devane@gmail.com>
2014-04-15 07:57:01 +0100

    RD: added raphael comments

--- js/checklist.js
+++ js/checklist.js
@@ -6,6 +6,42 @@
 
         $('.submit-button').addClass('hidden');
 
+        //grab the house
+
+        //var house = Raphael($('#house')[0]);
+//        var circle = house.circle(100,100,100);
+//        circle.attr("fill", "#f00");
+//        circle.attr("stroke", "#fff");
+
+        var param = {stroke: "#fff", "stroke-width": 30};
+
+        var R = 200;
+
+//        house.customAttributes.arc = function (value, total, R) {
+//            var alpha = 360 / total * value,
+//                a = (90 - alpha) * Math.PI / 180,
+//                x = 300 + R * Math.cos(a),
+//                y = 300 - R * Math.sin(a),
+//                color = "hsb(".concat(Math.round(R) / 200, ",", value / total, ", .75)"),
+//                path;
+//            if (total == value) {
+//                path = [["M", 300, 300 - R], ["A", R, R, 0, 1, 1, 299.99, 300 - R]];
+//            } else {
+//                path = [["M", 300, 300 - R], ["A", R, R, 0, +(alpha > 180), 1, x, y]];
+//            }
+//            return {path: path, stroke: color};
+//        };
+
+        //var sec = house.path().attr(param).attr({arc: [30, 60, R]});
+
+
+        //convert svg to raphael
+        //http://www.readysetraphael.com/
+        //http://parall.ax/blog/view/2985/tutorial-creating-an-interactive-svg-map
+
+        //load svg
+
+        //http://raphaeljs.com/reference.html#Raphael
 
 
     } else {
